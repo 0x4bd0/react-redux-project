d6cf4cdfb5350485bba9a49ba102a5ebfdd4fc62
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-03-12 12:28:19 +0100

    add a basic validation

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -4,21 +4,29 @@ import { useDispatch, useSelector } from "react-redux";
 const AddBookC = () => {
     const dispatch = useDispatch()
     const state = useSelector((state) => state.addBook)
+    const [showErr, setShowErr] = useState(false)
     const [input, setInput] = useState('')
 
     const addBookButtonClicked = () => {
-        dispatch({
-            type: 'ADD_BOOK',
-            payload: {
-                title: input
-            }
-        })
+        if (input.length > 0) {
+            dispatch({
+                type: 'ADD_BOOK',
+                payload: {
+                    title: input
+                }
+            })
+            setShowErr(false)
+            setInput("")
+        } else {
+            setShowErr(true)
+        }
     }
 
     return (
 
         state.showForm && < div className="addBookContainer" >
-            <input type="text" onChange={(e) => setInput(e.target.value)} className="addBookInput" placeholder="book name here" />
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="addBookInput" placeholder="book name here" />
+            {showErr && <p className="nameValidation">A name is required</p>}
             <button className="addBookSubmit" onClick={() => addBookButtonClicked()}>+</button>
         </div >
 
